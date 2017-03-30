a164c039cdac44d704e0049157bfb6d7f55f4306
Nicholas  Vorraso <nvorraso2020@gmail.com>
2017-03-29 22:01:52 -0400

    server.js modifications

--- server.js
+++ server.js
@@ -2,16 +2,26 @@ const express = require('express');
 const port = process.env.PORT || 3000;
 const app = express();
 
+app.use("/:url", ( req, res, next ) => {
+    console.log( req.originalUrl , "===> Params")
 
-app.get( "/:url" , ( request, response ) => {
+   const url_encoded = encodeURIComponent(req.params.url) + "/" + req.url;
+   //req.protocol = "";
+   req.params.url = url_encoded;
 
-  const url_source = request.params;
+   console.log( req.params.url, "===> URL is encoded! ! !");
+   next();
+});
 
-  console.log( url.url, request.params );
+app.get( "/:url" , ( req, res, next ) => {
+  console.log( req.params , "===> GET RECIEVED");
 
-
-
-  response.end();
+  /*if ( req.url.indexOf("http/:") !== -1 || req.url.indexOf("https/:") !== -1 ) {
+    res.write("Invalid URL");
+  }
+  //if( url.indexOf( ".com" ) !== -1 ) {};
+  */
+  res.end();
 }).listen( port , () => {
    console.log( "listening on port " + port )
 });
