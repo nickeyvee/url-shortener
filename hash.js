a66c78facc9493f8551d85c072d1877f1ced48f2
Nicholas  Vorraso <nvorraso2020@gmail.com>
2017-04-01 23:39:56 -0400

    minor changes

---
+++ hash.js
@@ -0,0 +1,16 @@
+// create a function that returns a string of 4 randomly selcted
+// integers from the list of characters "char".
+module.exports = {
+  shorten: () => {
+    const randomIndex = () => { return (Math.floor(Math.random() * (34 - 1)  + 1)) };
+    const char = "123456789abcdefghijkmnopqrstuvwxyz";
+    let shortUrl = [];
+    let i = 4;
+
+    while( i > 0 ) {
+      shortUrl.push( char.charAt( randomIndex() ));
+      i--;
+    }
+    short_url = shortUrl.join("");
+  }
+}
